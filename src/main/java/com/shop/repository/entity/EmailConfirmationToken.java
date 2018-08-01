a80254914a4520329f59c41e4024d504ee98c828
Artur <buzov.artur@gmail.com>
2018-08-01 16:32:08 +0300

    email confirmation

---
+++ src/main/java/com/shop/repository/entity/EmailConfirmationToken.java
@@ -0,0 +1,68 @@
+package com.shop.repository.entity;
+
+import com.shop.repository.entity.base.DomainObject;
+
+import javax.persistence.*;
+import java.sql.Timestamp;
+import java.util.Calendar;
+import java.util.Date;
+
+@Entity
+public class EmailConfirmationToken extends DomainObject {
+
+    private static final int EXPIRATION = 60 * 24;
+
+    private String token;
+
+    @OneToOne(targetEntity = User.class, fetch = FetchType.EAGER)
+    @JoinColumn(nullable = false, name = "user_id")
+    private User user;
+
+    private Date expiryDate;
+
+    private Date calculateExpiryDate(int expiryTimeInMinutes) {
+        Calendar cal = Calendar.getInstance();
+        cal.setTime(new Timestamp(cal.getTime().getTime()));
+        cal.add(Calendar.MINUTE, expiryTimeInMinutes);
+        return new Date(cal.getTime().getTime());
+    }
+
+    public EmailConfirmationToken() {
+    }
+
+    public EmailConfirmationToken(String token, User user) {
+        this.token = token;
+        this.user = user;
+    }
+
+    public EmailConfirmationToken(String token, User user, Date expiryDate) {
+        this.token = token;
+        this.user = user;
+        this.expiryDate = expiryDate;
+    }
+
+    public String getToken() {
+        return token;
+    }
+
+    public void setToken(String token) {
+        this.token = token;
+    }
+
+    public User getUser() {
+        return user;
+    }
+
+    public void setUser(User user) {
+        this.user = user;
+    }
+
+    public Date getExpiryDate() {
+        return expiryDate;
+    }
+
+    public void setExpiryDate(Date expiryDate) {
+        this.expiryDate = expiryDate;
+    }
+}
+
