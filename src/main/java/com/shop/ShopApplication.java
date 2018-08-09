e25cb6940569d17400bb6fe8cbcd682fc42cc3a4
Java <java.quasar@gmail.com>
2018-08-10 02:06:38 +0300

    password validation

--- src/main/java/com/shop/ShopApplication.java
+++ src/main/java/com/shop/ShopApplication.java
@@ -10,8 +10,10 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.ApplicationPidFileWriter;
 import org.springframework.boot.context.event.ApplicationReadyEvent;
 import org.springframework.context.event.EventListener;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
+@EnableScheduling
 public class ShopApplication implements CommandLineRunner {
 
     //@Value("${is.prod}")
